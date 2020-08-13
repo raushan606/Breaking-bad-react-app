d1678c5cc91d2d65a7828651fdfdfe706208ae9d
Raushan <raushankumar606@gmail.com>
2020-08-13 22:10:10 +0530

    card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/ui/Header";
 import axios from "axios";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 const App = () => {
   const [items, setItems] = useState([]);
@@ -23,6 +24,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import CharacterItem from "./CharacterItem";
 
-const CharacterGrid = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const CharacterGrid = ({ items, isLoading }) => {
+  return isLoading ? (
+    <h1>Loading</h1>
+  ) : (
+    <section className="cards">
+      {items.map((item) => (
+        <CharacterItem key={item.char_id} item={item} />
+      ))}
+    </section>
+  );
+};
 
-export default CharacterGrid
+export default CharacterGrid;
